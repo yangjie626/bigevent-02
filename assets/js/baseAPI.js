d833828a049fc7aec3c98cb94034f022aabc0b4f
unknown <you@example.com>
2020-10-27 20:41:06 +0800

    第二天第二遍

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,11 @@
 var baseURL = 'http://ajax.frontend.itheima.net'
 $.ajaxPrefilter(function(option) {
     option.url = baseURL + option.url
+
+    if (option.url.indexOf('/my/') !== -1) {
+        // url含有my
+        option.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+$(function() {
+    getUserlist()
+})
+
+// 获取用户信息
+function getUserlist() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        /* headers: {
+            Authorization: localStorage.getItem('token')
+        }, */
+        success: function(res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            //调用成功则渲染图片名字
+            renderAvatar(res.data)
+        }
+    })
+}
+// 渲染图片名字
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎' + name)
+    var text = name[0].toUpperCase()
+    if (user.user_pic !== null) {
+        // 有照片
+        $('.text-avatar').hide()
+        $('.layui-nav-img').show().attr('src', user.user_pic)
+    } else {
+        $('.text-avatar').show().html(text)
+        $('.layui-nav-img').hide()
+    }
+
+
+}
