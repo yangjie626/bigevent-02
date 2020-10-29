95e13eca0c6549efe971b1a6cb3f0e0287037125
unknown <you@example.com>
2020-10-29 18:39:48 +0800

    index主页面完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,13 @@
 $(function() {
     getUserlist()
+    $('#btnLogout').on('click', function() {
+        layui.layer.confirm('确定退出么?', { icon: 3, title: '提示' }, function(index) {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+            layer.close(index);
+        });
+
+    })
 })
 
 // 获取用户信息
